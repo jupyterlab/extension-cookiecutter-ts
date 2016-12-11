6e9a60fd9e9d43172713d78b78fb7803603cd26f
Almar Klein <almar.klein@gmail.com>
2016-12-11 01:24:09 +0100

    Update setup.py

--- {{cookiecutter.extension_name}}/setup.py
+++ {{cookiecutter.extension_name}}/setup.py
@@ -12,7 +12,7 @@ if 'develop' in sys.argv or any(a.startswith('bdist') for a in sys.argv):
     import setuptools
 
 # Ensure that js has been built. This does not guarantee that the packages
-# are update to date. In the future we might do a more expensive check
+# are up to date. In the future we might do a more expensive check
 # involving file hashes, but only on sdist and bdist builds.
 if not os.path.exists('node_modules'):
     raise NodeModulesMissing("Before Python package can be installed or built, "
