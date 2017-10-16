d5b132662be9051d71c09c7fac5f0511d6bf8ab5
Steven Silvester <steven.silvester@ieee.org>
2017-10-16 10:48:58 -0500

    Include style by default

--- {{cookiecutter.extension_name}}/src/index.ts
+++ {{cookiecutter.extension_name}}/src/index.ts
@@ -2,6 +2,9 @@ import {
   JupyterLabPlugin
 } from '@jupyterlab/application';
 
+import '../style/index.css';
+
+
 /**
  * Initialization data for the {{ cookiecutter.extension_name }} extension.
  */
