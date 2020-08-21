cff2046667829a9a3d1f00fd1f1c814043c43113
Zachary Sailer <zachsailer@gmail.com>
2020-08-21 08:23:01 -0700

    updated repo with jupyter_server changes (#74)
    * updated repo with jupyter_server changes
    
    * Remove ExtensionHandlerMixin
    
    ExtensionHandlerMixin's are only necessary for `ExtensionApp`.

--- {{cookiecutter.python_name}}/{{cookiecutter.python_name}}/handlers.py
+++ {{cookiecutter.python_name}}/{{cookiecutter.python_name}}/handlers.py
@@ -1,12 +1,12 @@
 import json
 
-from notebook.base.handlers import APIHandler
-from notebook.utils import url_path_join
+from jupyter_server.base.handlers import APIHandler
+from jupyter_server.utils import url_path_join
 import tornado
 
 class RouteHandler(APIHandler):
-    # The following decorator should be present on all verb methods (head, get, post, 
-    # patch, put, delete, options) to ensure only authorized user can request the 
+    # The following decorator should be present on all verb methods (head, get, post,
+    # patch, put, delete, options) to ensure only authorized user can request the
     # Jupyter server
     @tornado.web.authenticated
     def get(self):
@@ -17,7 +17,7 @@ class RouteHandler(APIHandler):
 
 def setup_handlers(web_app):
     host_pattern = ".*$"
-    
+
     base_url = web_app.settings["base_url"]
     route_pattern = url_path_join(base_url, "{{ cookiecutter.python_name }}", "get_example")
     handlers = [(route_pattern, RouteHandler)]
