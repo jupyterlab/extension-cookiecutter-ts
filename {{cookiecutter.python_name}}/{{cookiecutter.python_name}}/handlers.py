fc5ac5466b647d03b8b9fe06f601da83037c3e86
Zachary Sailer <zachsailer@gmail.com>
2020-08-21 08:15:04 -0700

    Remove ExtensionHandlerMixin
    ExtensionHandlerMixin's are only necessary for `ExtensionApp`.

--- {{cookiecutter.python_name}}/{{cookiecutter.python_name}}/handlers.py
+++ {{cookiecutter.python_name}}/{{cookiecutter.python_name}}/handlers.py
@@ -1,11 +1,10 @@
 import json
 
 from jupyter_server.base.handlers import APIHandler
-from jupyter_server.extension.handler import ExtensionHandlerMixin
 from jupyter_server.utils import url_path_join
 import tornado
 
-class RouteHandler(ExtensionHandlerMixin, APIHandler):
+class RouteHandler(APIHandler):
     # The following decorator should be present on all verb methods (head, get, post,
     # patch, put, delete, options) to ensure only authorized user can request the
     # Jupyter server
