333343ff4c1bd1327e9f6c10f2740a85a244cb9f
Jason Grout <jgrout6@bloomberg.net>
2020-10-29 11:06:17 -0700

    metadata.json -> install.json

--- {{cookiecutter.python_name}}/setup.py
+++ {{cookiecutter.python_name}}/setup.py
@@ -37,7 +37,7 @@ labext_name = "{{ cookiecutter.labextension_name }}"
 
 data_files_spec = [
     ("share/jupyter/labextensions/%s" % labext_name, lab_path, "**"),
-    ("share/jupyter/labextensions/%s" % labext_name, HERE, "metadata.json"),
+    ("share/jupyter/labextensions/%s" % labext_name, HERE, "install.json"),
     {%- if cookiecutter.has_server_extension == "y" -%}
     ("etc/jupyter/jupyter_server_config.d",
      "jupyter-config", "{{ cookiecutter.python_name }}.json"),
