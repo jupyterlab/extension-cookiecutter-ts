c5192852fefa8a3487c154b31750e81a5e877310
Frederic COLLONVAL <fcollonval@gmail.com>
2021-01-10 15:15:06 +0100

    Fix _version

--- {{cookiecutter.python_name}}/{{cookiecutter.python_name}}/_version.py
+++ {{cookiecutter.python_name}}/{{cookiecutter.python_name}}/_version.py
@@ -6,7 +6,7 @@ __all__ = ["__version__"]
 def _fetchVersion():
     HERE = Path(__file__).parent.resolve()
 
-    for settings in HERE.glob("package.json"): 
+    for settings in HERE.rglob("package.json"): 
         try:
             with settings.open() as f:
                 return json.load(f)["version"]
