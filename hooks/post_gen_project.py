9cd6d481a14448e9fbb8d4a8132819773304efe2
Frédéric Collonval <fcollonval@gmail.com>
2021-06-04 22:22:28 +0200

    Add template for settings files (#156)
    * Add template for settings files
    Fixes #90
    
    * Log settings.composite for clearer message
    
    * Linter code
    
    * Add CI test
    
    * Remove linter check for settings case

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
-import os
 from pathlib import Path
 
 PROJECT_DIRECTORY = Path.cwd()
 
 
-def remove_path(path):
+def remove_path(path: str) -> None:
+    """Remove the provided path.
+    
+    If the target path is a directory, remove it recursively.
+    """
     if path.is_file():
         path.unlink()
     elif path.is_dir():
@@ -15,6 +18,9 @@ def remove_path(path):
 
 
 if __name__ == "__main__":
+
+    if not "{{ cookiecutter.has_settings }}".lower().startswith("y"):
+        remove_path(PROJECT_DIRECTORY / "schema")
 
     if not "{{ cookiecutter.has_server_extension }}".lower().startswith("y"):
         for f in (
--- {{cookiecutter.python_name}}/src/index.ts
+++ {{cookiecutter.python_name}}/src/index.ts
@@ -1,18 +1,32 @@
 import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
-} from '@jupyterlab/application';{% if cookiecutter.has_server_extension.lower().startswith('y') %}
+} from '@jupyterlab/application';{% if cookiecutter.has_settings.lower().startswith('y') %}
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';{% endif %}{% if cookiecutter.has_server_extension.lower().startswith('y') %}
 
 import { requestAPI } from './handler';{% endif %}
 
 /**
  * Initialization data for the {{ cookiecutter.labextension_name }} extension.
  */
-const extension: JupyterFrontEndPlugin<void> = {
+const plugin: JupyterFrontEndPlugin<void> = {
   id: '{{ cookiecutter.labextension_name }}:plugin',
-  autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension {{ cookiecutter.labextension_name }} is activated!');{% if cookiecutter.has_server_extension.lower().startswith('y') %}
+  autoStart: true,{% if cookiecutter.has_settings.lower().startswith('y') %}
+  optional: [ISettingRegistry],{% endif %}
+  activate: (app: JupyterFrontEnd{% if cookiecutter.has_settings.lower().startswith('y') %}, settingRegistry: ISettingRegistry | null{% endif %}) => {
+    console.log('JupyterLab extension {{ cookiecutter.labextension_name }} is activated!');{% if cookiecutter.has_settings.lower().startswith('y') %}
+
+    if (settingRegistry) {
+      settingRegistry
+        .load(plugin.id)
+        .then(settings => {
+          console.log('{{ cookiecutter.labextension_name }} settings loaded:', settings.composite);
+        })
+        .catch(reason => {
+          console.error('Failed to load settings for {{ cookiecutter.labextension_name }}.', reason);
+        });
+    }{% endif %}{% if cookiecutter.has_server_extension.lower().startswith('y') %}
 
     requestAPI<any>('get_example')
       .then(data => {
@@ -26,4 +40,4 @@ const extension: JupyterFrontEndPlugin<void> = {
   }
 };
 
-export default extension;
+export default plugin;
