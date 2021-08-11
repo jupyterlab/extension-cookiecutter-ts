65cd04083bce592310317158bfd45a15c9c6fc2a
Frédéric Collonval <fcollonval@gmail.com>
2021-08-11 18:33:14 +0200

    Revert "Add pypi classifiers (#174)"
    This reverts commit fe5889e2b0423c0a8d60912a3da47dd0e3ce4c12.

--- {{cookiecutter.python_name}}/setup.py
+++ {{cookiecutter.python_name}}/setup.py
@@ -65,10 +65,6 @@ setup_args = dict(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Framework :: Jupyter",
-        "Framework :: Jupyter :: JupyterLab",
-        "Framework :: Jupyter :: JupyterLab :: 3",
-        "Framework :: Jupyter :: JupyterLab :: Extensions",
-        "Framework :: Jupyter :: JupyterLab :: Extensions :: Prebuilt",
     ],
 )
 
