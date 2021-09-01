34aa4050f547475b1eb0e323dcafcc44c0d09501
Michał Krassowski <5832902+krassowski@users.noreply.github.com>
2021-09-01 18:44:17 +0100

    Revert "Revert "Add pypi classifiers (#174)"" (#177)

--- {{cookiecutter.python_name}}/setup.py
+++ {{cookiecutter.python_name}}/setup.py
@@ -65,11 +65,10 @@ setup_args = dict(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Framework :: Jupyter",
-        # These classifiers will be valid when https://github.com/pypa/warehouse/pull/9882 gets merged
-        # "Framework :: Jupyter :: JupyterLab",
-        # "Framework :: Jupyter :: JupyterLab :: 3",
-        # "Framework :: Jupyter :: JupyterLab :: Extensions",
-        # "Framework :: Jupyter :: JupyterLab :: Extensions :: Prebuilt",
+        "Framework :: Jupyter :: JupyterLab",
+        "Framework :: Jupyter :: JupyterLab :: 3",
+        "Framework :: Jupyter :: JupyterLab :: Extensions",
+        "Framework :: Jupyter :: JupyterLab :: Extensions :: Prebuilt",
     ],
 )
 
