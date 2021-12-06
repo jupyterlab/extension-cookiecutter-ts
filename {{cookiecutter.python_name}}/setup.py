3a4a95c922879e6a3f6ed66add92443b1f9d532f
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-12-06 23:55:26 +0100

    Add Python 3.10 trove classifier

--- {{cookiecutter.python_name}}/setup.py
+++ {{cookiecutter.python_name}}/setup.py
@@ -71,6 +71,7 @@ setup_args = dict(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Framework :: Jupyter",
         "Framework :: Jupyter :: JupyterLab",
         "Framework :: Jupyter :: JupyterLab :: 3",
