5aa176bc668b163bea399bae26fe6f1b77a84976
Frédéric Collonval <fcollonval@gmail.com>
2022-02-12 16:46:50 +0100

    Fix pytest

--- {{cookiecutter.python_name}}/{{cookiecutter.python_name}}/tests/test_handlers.py
+++ {{cookiecutter.python_name}}/{{cookiecutter.python_name}}/tests/test_handlers.py
@@ -3,7 +3,7 @@ import json
 
 async def test_get_example(jp_fetch):
     # When
-    response = await jp_fetch("get_example")
+    response = await jp_fetch("{{ cookiecutter.python_name | replace('_', '-') }}", "get_example")
 
     # Then
     assert response.code == 200
