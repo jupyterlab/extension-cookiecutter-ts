07c09025434d714d2daf463bcdc568b5da709dc8
Frédéric Collonval <fcollonval@gmail.com>
2022-02-17 09:49:10 +0100

    Apply review suggestions - Handle server spin up in Playwright config - Cache browser download - Use one artifact - Don't constrain explicitly Python

--- {{cookiecutter.python_name}}/ui-tests/playwright.config.js
+++ {{cookiecutter.python_name}}/ui-tests/playwright.config.js
@@ -1,4 +1,14 @@
 /**
  * Configuration for Playwright using default from @jupyterlab/galata
  */
-module.exports = require('@jupyterlab/galata/lib/playwright-config');
+const baseConfig = require('@jupyterlab/galata/lib/playwright-config');
+
+module.exports = {
+  ...baseConfig,
+  webServer: {
+    command: 'jlpm start',
+    url: 'http://localhost:8888/lab',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+}
